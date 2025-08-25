bae7282d4557e187559ca430cda619a432aebad9
Emilse Arias <emilsearias@gmail.com>
2025-08-25 09:41:46 -0300

    mostramos los productos del backend en la pagina principal del proyecto

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,9 +1,26 @@
 import { Container, Row, Form } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
 import { useEffect, useState } from "react";
+import { listarProductos } from "../../helpers/queries";
 
-const Inicio = ({ productos }) => {
+const Inicio = () => {
   const [terminoBusqueda, setTerminoBusqueda] = useState("");
+  const [productos, setProductos] = useState([]); 
+
+   useEffect(() => {
+      obtenerProductos();
+    }, []);
+  
+    const obtenerProductos = async () => {
+      const respuesta = await listarProductos();
+      console.log(respuesta);
+      if(respuesta.status === 200){
+        const datos = await respuesta.json()
+        setProductos(datos)
+      }else{
+        console.log('Ocurrio un error al obtener los productos')
+      }
+    };
 
   const handleInputChange = (e) => {
     setTerminoBusqueda(e.target.value);
@@ -38,7 +55,7 @@ const Inicio = ({ productos }) => {
           {productosFiltrados.length > 0
             ? productosFiltrados.map((producto) => (
                 <CardProducto
-                  key={producto.id}
+                  key={producto._id}
                   producto={producto}
                 ></CardProducto>
               ))
